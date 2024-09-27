5cd04b937d661f7ac52dda0220d91f1376dd3008
B4869 <nether.demonic@gmail.com>
2024-09-27 14:52:52 +0700

    something

---
+++ src/data/data.js
@@ -0,0 +1,11 @@
+export const people = [
+  {
+    name: 'Leslie Alexander',
+    email: 'leslie.alexander@example.com',
+    role: 'Co-Founder / CEO',
+    imageUrl:
+      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    lastSeen: '3h ago',
+    lastSeenDateTime: '2023-01-23T13:23Z',
+  },
+]
